0ef0f332d0c5d0c5bbd95fb7851021a628d8e4c6
Tye Williams <114264890+Tye845@users.noreply.github.com>
2022-10-18 23:51:27 -0700

    Update GameLauncher.java

--- GameLauncher.java
+++ GameLauncher.java
@@ -7,10 +7,7 @@ public class GameLauncher
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration(); 
         config.width = 800;//set the width of your screen window
         config.height = 480; //set the height of your screen window
-        //keep these the same ratio as your WORLD UNITS!!!!!!!!!!
-        
-        
-        //Create an instance of the class that extends the Game class
+       
         LwjglApplication launcher = new LwjglApplication(new Pong(), config);
     }
 }
